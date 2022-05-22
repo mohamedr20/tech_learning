54db8c8ad00ecdc43bd1f86ce8c400ce62837a9f
Mohamed Isse <mohamedr201@gmail.com>
2022-05-22 18:09:38 -0400

    Delete exsisting entries when seeding

--- backend_design/library_management_app/db/seed/initial.ts
+++ backend_design/library_management_app/db/seed/initial.ts
@@ -2,6 +2,18 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
+  await knex("author").del();
+  await knex("books").del();
+  await knex("book_category").del();
+  await knex("book_item").del();
+  await knex("category").del();
+  await knex("fine").del();
+  await knex("library").del();
+  await knex("library_business_hours").del();
+  await knex("library_card").del();
+  await knex("rack").del();
+  await knex("roles").del();
+  await knex("user_roles").del();
   await knex("users").del();
 
   // Inserts seed entries
@@ -16,7 +28,7 @@ export async function seed(knex: Knex): Promise<void> {
       phone: "703-111-1234",
       date_of_birth: knex.fn.now(),
       created_at: knex.fn.now(),
-      updated_at: knex.fn.now(),
-    },
+      updated_at: knex.fn.now()
+    }
   ]);
 }
